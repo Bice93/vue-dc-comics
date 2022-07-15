7320b24c6f1a00555ddb44825e340d4304ab0373
Bice93 <bianca.cerulo@hotmail.it>
2022-07-15 16:04:56 +0200

    Header Style

--- src/App.vue
+++ src/App.vue
@@ -20,18 +20,13 @@ export default {
     MainHeader,
     MainMain,
     MainFooter,
-    
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./styles/general.scss";
+
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,59 @@
 <template>
   <header>
-        <h1>Header della Pagina</h1>
+    <div class="container">
+      <img src="../assets/images/dc-logo.png" alt="" />
+      <nav>
+        <ul>
+          <li>Characters</li>
+          <li class="activeElement">Comics</li>
+          <li>Movies</li>
+          <li>Tv</li>
+          <li>Games</li>
+          <li>Collectibles</li>
+          <li>Videos</li>
+          <li>Fans</li>
+          <li>News</li>
+          <li>Shop</li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped lang="scss">
+@import "../styles/variables.scss";
 
+header {
+  .container {
+    width: 1000px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+
+img {
+  height: 60px;
+}
+
+ul {
+  list-style: none;
+  li {
+    font-size: 10px;
+    font-weight: bold;
+    display: inline-block;
+    padding: 40px 10px;
+    text-transform: uppercase;
+
+    &.activeElement{
+        color: $colorActive;
+        border-bottom: 4px solid $colorActive;
+    }
+  }
+}
 </style>
